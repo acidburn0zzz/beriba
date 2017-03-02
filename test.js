b7b73e572406587652fbe68fa9f4ca56946515a6
Adrian Shum <adrianshum@ecargo.com>
2017-03-02 13:15:15 +0800

    putFromReadStream getToWriteStrea tests

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var test = require('tape')
+var tape = require('tape')
 var beriba = require('./')
 var fs = require('fs')
 var raco = require('raco')
@@ -18,19 +18,20 @@ var setup = (t, cb) => {
   })
 }
 
-var teardown = (t, cb) => {
-  rimraf(dir, cb)
-}
+var teardown = (t, cb) => rimraf(dir, cb)
 
-var wrap = (genFn) => (t) => {
-  setup(t, (err) => {
+var test = (title, genFn) => {
+  tape(title, (t) => setup(t, (err) => {
     t.error(err)
-    raco.wrap(genFn)(t, () => teardown(t, () => {}))
-  })
+    raco.wrap(genFn)(t, () => {
+      teardown(t, (err) => t.end(err))
+    })
+  }))
 }
 
-test('putFromFile getFromFile', wrap(function * (t, next) {
-  var blobs = beriba(dir + '/blobs')
+var blobs = beriba(dir + '/blobs')
+
+test('putFromFile getToFile', function * (t, next) {
   var testkey = 'abc/foo'
   t.equal(yield blobs.exists(testkey, next), false, 'blob not exists before put')
   var res = yield blobs.putFromFile(testkey, filepath, next)
@@ -46,5 +47,22 @@ test('putFromFile getFromFile', wrap(function * (t, next) {
   } catch (err) {
     t.ok(err.notFound, 'blob notFound error get after remove')
   }
-  t.end()
-}))
+})
+
+test('putFromReadStream getToWriteStream', function * (t, next) {
+  var testkey = 'def/foo'
+  t.equal(yield blobs.exists(testkey, next), false, 'blob not exists before put')
+  var res = yield blobs.putFromReadStream(testkey, fs.createReadStream(filepath), next)
+  t.equal(yield blobs.exists(testkey, next), true, 'blob exists after put')
+  t.equal(res.size, filedata.length, 'return file byte size after put')
+  yield blobs.getToWriteStream(testkey, fs.createWriteStream('./testdata/2'), next)
+  t.equal(yield fs.readFile('./testdata/2', 'utf8', next), filedata)
+  yield blobs.remove(testkey, next)
+  t.equal(yield blobs.exists(testkey, next), false, 'blob not exists after remove')
+  try {
+    yield blobs.getToWriteStream(testkey, fs.createWriteStream('./testdata/3'), next)
+    t.fail('should not process get not exists')
+  } catch (err) {
+    t.ok(err.notFound, 'blob notFound error get after remove')
+  }
+})
